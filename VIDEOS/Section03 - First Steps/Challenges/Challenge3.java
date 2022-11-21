49ec345eff259e070164540bcbcaa140a6c3f481
elektronomous <akbarfaza222@gmail.com>
2022-11-22 05:02:13 +0800

    Primitive Types

---
+++ VIDEOS/Section03 - First Steps/Challenges/Challenge3.java
@@ -0,0 +1,21 @@
+/* Create additional variables
+ * Declare the following variables and add to our program:
+ * 1 - mySecondNumber which is an int and assign a value of 12 to it
+ * 2 - myThirdNumber also an int with a value of 6
+ * 3 - total the 1 and 2 and save the result to myTotal which is an int.
+ */
+public class Challenge3 {
+    public static void main(String[] args) {
+        // 1
+        int mySecondNumber = 12;
+        // 2
+        int myThirdNumber = 6;
+        // 3
+        int myTotal = mySecondNumber + myThirdNumber;
+
+        System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
+        System.out.println(myTotal);
+
+    }
+}
