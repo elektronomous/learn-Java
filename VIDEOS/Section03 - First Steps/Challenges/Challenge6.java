9b3965bd209cbf24a250848822524bbcd6f655fb
elektronomous <akbarfaza222@gmail.com>
2022-12-07 23:47:58 +0800

    completed Challenge6

---
+++ VIDEOS/Section03 - First Steps/Challenges/Challenge6.java
@@ -0,0 +1,4 @@
+/* Create two variables in JShell
+ * 1 - One variable called mySecondNumber, which is an int, with a value of 12.
+ * 2 - Another variable called myThirdNumber, also of type int, with a value of 6.
+ */
