cefbc9dba7fdd6832045f976bf8f3ad1bbdbd3fd
elektronomous <akbarfaza222@gmail.com>
2022-12-08 01:39:29 +0800

    figure out the size of data types

---
+++ VIDEOS/Section03 - First Steps/Src/SizeDataTypes.java
@@ -0,0 +1,7 @@
+public class SizeDataTypes {
+    public static void main(String[] args) {
+        System.out.println("The integer has width: " + Integer.SIZE);
+        System.out.println("The Long has widht: " + Long.SIZE);
+        // and so on.
+    }
+}
