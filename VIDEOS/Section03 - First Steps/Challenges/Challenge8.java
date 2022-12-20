165bd5e224399cc9d793a0808649fc42efdb6f0d
elektronomous <akbarfaza222@gmail.com>
2022-12-21 05:25:51 +0800

    done Challenge8

---
+++ VIDEOS/Section03 - First Steps/Challenges/Challenge8.java
@@ -0,0 +1,17 @@
+/* Create three char variables to store the character for the question-mark symbol
+ * mySimpleChar should be assigned the literal question-mark character ?.
+ * myUnicodeChar should be assigned the unicode value for the question-mark character ?.
+ * myDecimalChar should be assigned the decimal value for the question-mark character
+ */
+
+public class Challenge8 {
+    public static void main(String[] args) {
+        char mySimpleChar = '?';
+        char myUnicodeChar = '\u003F';
+        char myDecimalChar = 63;
+
+        System.out.println(mySimpleChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myDecimalChar);
+    }
+}
