044c43be54dc466dd986bbfd32bea4c1ff474065
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2022-12-20 18:18:18 +0800

    using AND logical operator

--- VIDEOS/Section03 - First Steps/Src/Hello.java
+++ VIDEOS/Section03 - First Steps/Src/Hello.java
@@ -1,5 +1,26 @@
 public class Hello { 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // using if-then statement
+        boolean isAlien = false;
+
+        if (isAlien == false) {
+            System.out.println("There's an alien out there.");
+            System.out.println("Iam scared with the alien.");
+        }
+
+        // using AND logical operator
+        int topScore = 80;
+
+        if (topScore < 100) {
+            System.out.println("You get the high score.");
+        }
+
+        int secondTopScore = 81;
+
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than topScore and less than 100");
+        }
     }
 }
