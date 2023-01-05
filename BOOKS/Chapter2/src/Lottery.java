cd7c2b862ffb961a6df23a871a04f9314022e30f
elektronomous <akbarfaza222@gmail.com>
2023-01-05 02:17:04 -0800

    RQ sect 2.6 DONE

---
+++ BOOKS/Chapter2/src/Lottery.java
@@ -0,0 +1,13 @@
+public class Lottery {
+    public static void main(String[] args) {
+        int winningNumber = 973;
+
+        System.out.print("The winning number ");
+        System.out.print("in today's lottery is ");
+        System.out.println(winningNumber);
+        /* 
+        int i;
+        System.out.println(i); 
+        */
+    }
+}
