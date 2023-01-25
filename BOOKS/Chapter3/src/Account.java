79d1e5b66c939f16dc5bd3c8c94ddb4f07de911f
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2023-01-25 18:51:49 +0800

    Chapter3: RQ 3.3 DONE

---
+++ BOOKS/Chapter3/src/Account.java
@@ -0,0 +1,32 @@
+public class Account {
+    // variable-declarations
+    // instance-variables
+    private double balance;
+
+    // constructor-declarations
+    public Account(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public Account() {
+        balance = 0.0;
+    }
+
+    // method-declaration
+    // instance-methods
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void close() {
+        balance = 0.0;
+    }
+}
