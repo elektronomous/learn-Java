c9eb9532bf9ef1447d4908daa4435cef0a93c2ee
elektronomous <akbarfaza222@gmail.com>
2023-05-06 02:26:35 +0800

    Section2.5 DONE

---
+++ BOOKS/Chapter2/src/Debugging.java
@@ -0,0 +1,6 @@
+public class Debugging {
+    public static void main(String[] args) {
+        int i;
+        System.out.println(i);
+    }
+}
---
+++ BOOKS/Chapter2/src/Lottery.java
@@ -0,0 +1,8 @@
+public class Lottery {
+    public static void main(String[] args) {
+        int winningNumber = 973;
+        System.out.print("The winning number ");
+        System.out.print("in today's lottery is ");
+        System.out.println(winningNumber);
+    }
+}
