6389f311ed1bd953f5924a0e631d0eb2903b33c8
elektronomous <akbarfaza222@gmail.com>
2023-05-07 00:32:23 +0800

    Project3 On GOing

---
+++ BOOKS/Chapter2/Projects/Project_2.java
@@ -0,0 +1,14 @@
+public class Project_2 {
+    public static void main(String[] args) {
+        System.out.println("                             _");
+        System.out.println("        ´     ´             [ ]");
+        System.out.println("       (\\____/)            (   )");
+        System.out.println("        (_oo_)              |>|");
+        System.out.println("         (O)             __/===\\__");
+        System.out.println("       __||__    \\)     //| o=o |\\\\");
+        System.out.println("    []/______\\[] /    <]  | o=o |  [>");
+        System.out.println("    / \\______/ \\/         \\=====/");
+        System.out.println("   /    /__\\             / / | \\ \\");
+        System.out.println("  (\\   /____\\         <_____________>");
+    }
+}
---
+++ BOOKS/Chapter2/Projects/Project_3.java
@@ -0,0 +1,5 @@
+public class Project_3 {
+    public static void main(String[] args) {
+        
+    }
+}
