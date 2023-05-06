32ac94da37fe8a88f848f45c6b303d558fc35b53
elektronomous <akbarfaza222@gmail.com>
2023-05-07 01:22:26 +0700

    Project_3 DONE

--- BOOKS/Chapter2/Projects/Project_3.java
+++ BOOKS/Chapter2/Projects/Project_3.java
@@ -1,15 +1,15 @@
 public class Project_3 {
     public static void main(String[] args) {
-        System.out.println("      ____          ____          ____           ____");
-        System.out.println("     /\   \        /\   \        /\___\         /\   \");
-        System.out.println("     \:\   \      /::\   \      /:/   /        /::\   \");
-        System.out.println("      ");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("      ____            ____              ____             ____");
+        System.out.println("     /\\   \\          /\\   \\            /\\___\\           /\\   \\");
+        System.out.println("     \\:\\   \\        /::\\   \\          /:/   /          /::\\   \\");
+        System.out.println(" ____/::\\   \\      /:/\\:\\   \\        /:/   /          /:/\\:\\   \\");
+        System.out.println("/\\  /:/\\:\\___\\    /::\\_\\:\\   \\      /:/___/  ___     /::\\_\\:\\   \\");
+        System.out.println("\\ \\/:/  \\/___/   /:/\\:\\ \\:\\   \\     |:|   | /\\__\\   /:/\\:\\ \\:\\   \\");
+        System.out.println(" \\/:/   /        \\/__\\:\\/:/   /     |:|   |/:/  /   \\/__\\:\\/:/   /");
+        System.out.println("  \\/___/              \\::/   /      |:|___/:/  /         \\::/   /");
+        System.out.println("                      /:/   /        \\:::::/__/          /:/   /");
+        System.out.println("                     /:/   /          -----             /:/   /");
+        System.out.println("                     \\/___/                             \\/___/");
     }
 }
