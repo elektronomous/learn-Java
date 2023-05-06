fbb2eff607ffac6744a75fc031bfed4f3485f8a3
elektronomous <akbarfaza222@gmail.com>
2023-05-07 01:52:02 +0800

    Project_3 on GOING

--- BOOKS/Chapter2/Projects/Project_3.java
+++ BOOKS/Chapter2/Projects/Project_3.java
@@ -1,5 +1,15 @@
 public class Project_3 {
     public static void main(String[] args) {
-        
+        System.out.println("      ____          ____          ____           ____");
+        System.out.println("     /\   \        /\   \        /\___\         /\   \");
+        System.out.println("     \:\   \      /::\   \      /:/   /        /::\   \");
+        System.out.println("      ");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
 }
