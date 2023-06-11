d97e64a35d29011be711ed487474b2c0457ff65f
elektronomous <akbarfaza222@gmail.com>
2023-06-11 21:13:02 +0800

    Exercises DONE

---
+++ BOOKS/INTRODUCTION_TO_JAVA_PROGRAMMING_AND_DATA_STRUCTURES/Chapter1/Exercises/Exercise_11.java
@@ -0,0 +1,19 @@
+package BOOKS.INTRODUCTION_TO_JAVA_PROGRAMMING_AND_DATA_STRUCTURES.Chapter1.Exercises;
+
+public class Exercise_11 {
+    public static void main(String[] args){
+        // the next 5 years
+        System.out.println("One birth every 7 seconds");
+        System.out.print("(365 * 5 * 24 * 3600) / 7 = ");
+        System.out.print((365 * 5 * 24 * 3600) / 7);
+
+        System.out.println("One death every 13 seconds");
+        System.out.print("(365 * 5 * 24 * 3600) / 13 = ");
+        System.out.print((365 * 5 * 24 * 3600) / 13);
+
+        System.out.println("One new immigrant every 45 seconds");
+        System.out.print("(365 * 5 * 24 * 3600) / 45 = ");
+        System.out.print((365 * 5 * 24 * 3600) / 45);
+
+    }
+}
