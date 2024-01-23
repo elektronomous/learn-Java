2019f047de7ef5ee27fcd441c18d649432c02b4e
elektronomous <akbarfaza222@gmail.com>
2024-01-24 06:49:38 +0800

    CHAPTER1 DONE

---
+++ BOOKS/Intro_Java_and_Data_Structures/Ch1 - Introduction to Computers Programs and Java/ProgrammingExercises/Exercise1_5.java
@@ -0,0 +1,9 @@
+/* (Compute expressions) Write a program that displays the result of
+ * ((9.5 x 4.5) - (2.5 x 3)) / (45.5 - 3.5)
+ */
+
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+}
---
+++ BOOKS/Intro_Java_and_Data_Structures/Ch1 - Introduction to Computers Programs and Java/ProgrammingExercises/Exercise1_8.java
@@ -0,0 +1,15 @@
+/* (Area and perimeter of a circle) Write a program that displays the area and pe-
+ * rimeter of a circle that has a radius of 5.5 using the following formulas:
+ * 
+ *      perimeter = 2 x radius x phi
+ *      area = radius x radius x phi
+ */
+
+public class Exercise1_8 {
+    public static void main(String[] args) {
+        System.out.print("Perimeter = ");
+        System.out.println(2 * 5.5 * 3.14);
+        System.out.print("area = ");
+        System.out.println(5.5 * 5.5 * 3.14);
+    }
+}
