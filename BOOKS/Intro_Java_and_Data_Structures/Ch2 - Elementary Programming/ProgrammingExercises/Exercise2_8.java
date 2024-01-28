be979a1ec81438d8a7bc8e8445f72feaf3d50384
elektronomous <akbarfaza222@gmail.com>
2024-01-28 12:06:19 +0800

    Exercise2_10 in ProgrammingExercise Ch2 DONE

--- BOOKS/Intro_Java_and_Data_Structures/Ch2 - Elementary Programming/ProgrammingExercises/Exercise2_8.java
+++ BOOKS/Intro_Java_and_Data_Structures/Ch2 - Elementary Programming/ProgrammingExercises/Exercise2_8.java
@@ -5,4 +5,46 @@ time zone offset to GMT and displays the time in the specified time zone. Here i
 a sample run:
 Enter the time zone offset to GMT: -5
 The current time is 4:50:34
+
+The time    : 4:32:54  | offset = -5
+the current : 11:32:54 | offset = -5  
  */
+
+import java.util.Scanner;
+
+public class Exercise2_8 {
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user to enter the GMT offset
+        System.out.print("Enter GMT Offset: ");
+        int offsetGMT = input.nextInt();
+
+
+        // Get the Miliseconds of the current time
+        long totalMilliSeconds = System.currentTimeMillis();
+
+        // Obtain the total seconds since midnight, Jan 1, 1970
+        long totalSeconds = totalMilliSeconds / 1000;
+
+        // Get the current seconds 
+        long currentSeconds = totalSeconds % 60;
+
+        // make the total seconds to store the minute of the total seconds
+        totalSeconds /= 60;
+
+        // get the current minutes from the total seconds
+        long currentMinutes = totalSeconds % 60;
+
+        // make the total seconds to store the hour of the minute it stored earlier
+        totalSeconds /= 60;
+
+        // now get the current hours
+        long currentHours = (totalSeconds + offsetGMT) % 24;
+
+        // Display the result first
+        System.out.println("GMT: " + currentHours + ":" + currentMinutes + ":" + currentSeconds);
+
+    }
+}
